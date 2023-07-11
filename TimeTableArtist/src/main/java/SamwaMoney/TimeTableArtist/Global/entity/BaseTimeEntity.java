16fd5dec0cebfa1866fc357bc84328b38f22e81f
Minseo Cho <min1130@ewhain.net>
2023-07-12 01:56:05 +0900

    feat: 엔티티 생성

---
+++ TimeTableArtist/src/main/java/SamwaMoney/TimeTableArtist/Global/entity/BaseTimeEntity.java
@@ -0,0 +1,19 @@
+package SamwaMoney.TimeTableArtist.Global.entity;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+}
