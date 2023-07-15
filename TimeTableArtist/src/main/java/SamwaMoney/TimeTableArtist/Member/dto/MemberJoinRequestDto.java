e9c5cf325d75cf46a7c6572ec8022582a255fb06
Haeun Noh <00blowup@gmail.com>
2023-07-15 09:07:20 +0900

    feat: 회원가입, 로그인, 회원탈퇴 기능 추가

---
+++ TimeTableArtist/src/main/java/SamwaMoney/TimeTableArtist/Member/dto/MemberJoinRequestDto.java
@@ -0,0 +1,13 @@
+package SamwaMoney.TimeTableArtist.Member.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class MemberJoinRequestDto {
+
+    private String username;
+    private String password;
+
+}
