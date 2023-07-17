231dbbb09f5e80954fa2865121da0c8430e59d80
Minseo Cho <min1130@ewhain.net>
2023-07-17 13:07:44 +0900

    Merge branch 'develop' of https://github.com/SamwaMoney/Time-Table-Artist-back into feature/Timetable

--- TimeTableArtist/src/main/java/SamwaMoney/TimeTableArtist/Timetable/domain/Timetable.java
+++ TimeTableArtist/src/main/java/SamwaMoney/TimeTableArtist/Timetable/domain/Timetable.java
@@ -9,7 +9,6 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.ColumnDefault;
 
 import javax.persistence.*;
 import java.util.ArrayList;
