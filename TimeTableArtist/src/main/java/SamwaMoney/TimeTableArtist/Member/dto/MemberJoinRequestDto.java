48209d9dd9fbe846bb9dc36eb02ab86861053008
aoqlsdl <bini92444@gmail.com>
2023-07-18 00:05:36 +0900

    fix: merge 당시 발생한 에러 수정

--- TimeTableArtist/src/main/java/SamwaMoney/TimeTableArtist/Member/dto/MemberJoinRequestDto.java
+++ TimeTableArtist/src/main/java/SamwaMoney/TimeTableArtist/Member/dto/MemberJoinRequestDto.java
@@ -2,9 +2,10 @@ package SamwaMoney.TimeTableArtist.Member.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@AllArgsConstructor
+@NoArgsConstructor
 public class MemberJoinRequestDto {
 
     private String username;
