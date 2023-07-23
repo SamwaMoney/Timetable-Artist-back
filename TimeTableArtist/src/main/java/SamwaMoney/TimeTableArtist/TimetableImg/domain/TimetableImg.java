1c2c35c92f3e589ec69974e83efdf40729f8ae94
Sojeong Lee <sj27419@gmail.com>
2023-07-24 00:23:36 +0900

    add TimetableImg.java

--- TimeTableArtist/src/main/java/SamwaMoney/TimeTableArtist/TimetableImg/domain/TimetableImg.java
+++ TimeTableArtist/src/main/java/SamwaMoney/TimeTableArtist/TimetableImg/domain/TimetableImg.java
@@ -1,4 +1,22 @@
 package SamwaMoney.TimeTableArtist.TimetableImg.domain;
 
+import SamwaMoney.TimeTableArtist.Timetable.domain.Timetable;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="TimetableImg")
+@Getter
+@Setter
 public class TimetableImg {
+    @Id
+    @GeneratedValue
+    @Column(name="timetableImg_id")
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name="timetable_id")
+    private Timetable timetable;
 }
