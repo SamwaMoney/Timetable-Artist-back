ca7a8ce490485e1502067c62458ce18ee6cd6854
Soyeon-Cha <7103sy@naver.com>
2023-08-01 20:51:39 +0900

    fix: repository 수정

--- TimeTableArtist/src/main/java/SamwaMoney/TimeTableArtist/Comment/repository/PhotoRepository.java
+++ TimeTableArtist/src/main/java/SamwaMoney/TimeTableArtist/Comment/repository/PhotoRepository.java
@@ -3,6 +3,6 @@ package SamwaMoney.TimeTableArtist.Comment.repository;
 import SamwaMoney.TimeTableArtist.Comment.entity.Photo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PhotoRepository extends JpaRepository {
+public interface PhotoRepository extends JpaRepository <Photo, Long> {
     Photo findByPhotoId(Long photoId);
 }
